3cd516a29191d14ac115e8b6e3c02a7e3775bed1
RomenPoirierTaksev <73566899+RomenPoirierTaksev@users.noreply.github.com>
2021-09-22 14:42:26 -0400

    Added files

---
+++ playback.js
@@ -0,0 +1,20 @@
+const videoPlayer = document.querySelector('video');
+var ready = false;
+if (videoPlayer != null) {
+            videoPlayer.addEventListener('loadeddata', function() {
+                ready = true;
+            });
+        }
+
+chrome.runtime.onMessage.addListener((msg,sender,response) =>{
+
+    if(msg.name == "sendTime" && ready){
+        minutes = parseInt(msg.data[0].toString().substring(0,1));
+        seconds = parseInt(msg.data[0].toString().substring(2,4));
+        console.log(minutes, seconds);
+        videoPlayer.currentTime = minutes * 60 + seconds;
+    }
+});
+
+
+
---
+++ popup.js
@@ -0,0 +1,32 @@
+window.onload = (event) =>{
+   
+    getCrunchyRollTab();
+}
+
+function getCrunchyRollTab(){
+    chrome.tabs.query({currentWindow: true, active:true}, function(tabs){
+    let crunchyRollTab;
+    
+    if(tabs[0].url.includes("https://www.crunchyroll.com/one-piece/episode")){
+        console.log("here");
+        crunchyRollTab = tabs[0].url;
+        chrome.tabs.sendMessage(tabs[0].id, {name: "runScrape", data: crunchyRollTab});
+    }
+});
+}
+
+chrome.runtime.onMessage.addListener((msg, sender, res) =>{
+    if(msg.name == "time got"){
+        time_list = msg.data.toString().split(" ");
+        document.getElementById("time").innerHTML = "Time: " + time_list[0];
+        document.getElementById("name").innerHTML = "Thank you to " + time_list[1] + "!";
+        chrome.tabs.query({currentWindow: true, active:true}, function(tabs){
+                chrome.tabs.sendMessage(tabs[0].id, {name: "sendTime", data: time_list});
+        });
+    }
+    if(msg.name == "time got again"){
+        time_list = msg.data.toString().split(" ");
+        document.getElementById("time").innerHTML = "Time: " + time_list[0];
+        document.getElementById("name").innerHTML = "Thank you to " + time_list[1] + "!";
+    }
+});
