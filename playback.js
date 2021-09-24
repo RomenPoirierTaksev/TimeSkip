890f72c77153d76bd01c147d474b7324f15cbd9b
RomenPoirierTaksev <73566899+RomenPoirierTaksev@users.noreply.github.com>
2021-09-24 14:42:02 -0400

    fixed bugs

--- playback.js
+++ playback.js
@@ -11,8 +11,10 @@ chrome.runtime.onMessage.addListener((msg,sender,response) =>{
     if(msg.name == "sendTime" && ready){
         minutes = parseInt(msg.data[0].toString().substring(0,1));
         seconds = parseInt(msg.data[0].toString().substring(2,4));
-        console.log(minutes, seconds);
+        //console.log(msg.data)
+        //console.log(minutes, seconds);
         videoPlayer.currentTime = minutes * 60 + seconds;
+        //chrome.runtime.sendMessage({name: "time", data: msg.data})
     }
 });
 
--- popup.js
+++ popup.js
@@ -1,14 +1,11 @@
-window.onload = (event) =>{
-   
-    getCrunchyRollTab();
-}
+getCrunchyRollTab();
 
 function getCrunchyRollTab(){
     chrome.tabs.query({currentWindow: true, active:true}, function(tabs){
     let crunchyRollTab;
     
     if(tabs[0].url.includes("https://www.crunchyroll.com/one-piece/episode")){
-        console.log("here");
+        //onsole.log("here");
         crunchyRollTab = tabs[0].url;
         chrome.tabs.sendMessage(tabs[0].id, {name: "runScrape", data: crunchyRollTab});
     }
@@ -16,17 +13,17 @@ function getCrunchyRollTab(){
 }
 
 chrome.runtime.onMessage.addListener((msg, sender, res) =>{
+    time_list = msg.data.toString().split(" ");
     if(msg.name == "time got"){
-        time_list = msg.data.toString().split(" ");
         document.getElementById("time").innerHTML = "Time: " + time_list[0];
         document.getElementById("name").innerHTML = "Thank you to " + time_list[1] + "!";
         chrome.tabs.query({currentWindow: true, active:true}, function(tabs){
-                chrome.tabs.sendMessage(tabs[0].id, {name: "sendTime", data: time_list});
+            chrome.tabs.sendMessage(tabs[0].id, {name: "sendTime", data: time_list});
         });
     }
-    if(msg.name == "time got again"){
-        time_list = msg.data.toString().split(" ");
+    if(msg.name == "time"){
         document.getElementById("time").innerHTML = "Time: " + time_list[0];
         document.getElementById("name").innerHTML = "Thank you to " + time_list[1] + "!";
+        
     }
 });
